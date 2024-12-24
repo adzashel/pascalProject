1c7de84cc49bca184a942b6033e7f1792515974b
adzashel <seigneur.noir15@gmai.com>
2024-12-24 14:25:24 +0700

    half done

--- index.pas
+++ index.pas
@@ -17,80 +17,84 @@ begin
 
   case option of
     '1': begin
-        writeln('Number or stars ? ');
+        WriteLn('Choose stars or numbers ? ');
         WriteLn('1. Stars');
-        WriteLn('2. Numbers');
+        WriteLn('2. Numbers ');
         ReadLn(option2);
-        
+
         if option2 = '1' then
           begin
             WriteLn('Enter the number of rows : '); ReadLn(rows);
-            for i := 0 to rows - 1 do 
-              begin
-                for j := 0 to i do
-                    Write('*');
-                    WriteLn;              
-              end;
-          end
-          else 
-            begin
-              Write('Enter the number of rows');
-              ReadLn(rows);
-              i := 1;
-              while i <= rows do
+              i := 0;
+              while  i <= rows -1  do
                 begin
-                  j := 1;
-                  while j <= i do
-                  begin
-                    Write(j:3);
-                    j := j + 1;
-                  end;
-                  WriteLn;
-                  i := i + 1;
+                  j := 0;
+                  while  j <= i do
+                    begin
+                      Write('*');
+                      j := j + 1;
+                    end;
+                    WriteLn;
+                    i := i + 1;
                 end;
+          end
+          else if option2 = '2' then
+            begin
+              WriteLn('Enter the number of rows : '); ReadLn(rows);
+              for i := 0 to rows - 1 do 
+                begin
+                  for j := 0 to i do
+                    Write(j:3);
+                    WriteLn;
+                end;
+            end
+            else 
+            begin
+              WriteLn('Invalid option')
             end;
-      end;
+    end;
     '2': 
     begin
-    // diamond
-      WriteLn('Choose number of stars : ');
-      WriteLn('1.Stars ');
-      WriteLn('2.Numbers');
+      Writeln('Choose number or stars : ');
+      WriteLn('1. Stars ');
+      WriteLn('2. Number ');
       ReadLn(option2);
-      
+
       if option2 = '1' then
         begin
-          WriteLn(' Enter number of rows :'); ReadLn(rows);
+          WriteLn('Enter the number of rows : '); ReadLn(rows);
           i := 1;
+          // print upper half of diamond
           while i <= rows  do
             begin
               j := 1;
-              while  j <= rows - i do
+              while j <= rows - i do
                 begin
                   Write(' ');
                   j := j + 1;
                 end;
-              // print stars
-              j := 1;
-              while j <= 2 * i - 1 do
-                begin
-                  Write('*');
-                  j := j + 1;
-                end;
-              WriteLn;
-              i := i + 1;
+                // print stars
+                j := 1;
+                while j <= 2 * i - 1 do
+                  begin
+                    Write('*');
+                    j := j + 1;
+                  end;
+                  WriteLn;
+                  i := i + 1;
             end;
 
-            // lower diamond
+            // print lower half of diamond
             i := rows - 1;
-            while  i >= 1 do
+            while  i >= 1 do 
               begin
                 j := 1;
-                while j <= rows - i do
+                while j <= rows - i  do
                   begin
                     Write(' ');
                     j := j + 1;
                   end;
+                  // print stars
                   j := 1;
                   while j <= 2 * i - 1 do
                     begin
@@ -100,31 +104,92 @@ begin
                   WriteLn;
                   i := i - 1;
               end;
-        end;
+        end
+        else if option2 = '2' then
+          begin
+            Writeln('Enter number of rows: '); Readln(rows);
+            // upper half of diamond
+            i := 1;
+            while i <= rows do
+              begin
+                j := 1;
+                while j <= rows - i do
+                  begin
+                    Write(' ');
+                    j := j + 1;
+                  end;
+                  // print numbers
+                  j := 1;
+                  while j <= 2 * i - 1 do
+                    begin
+                      Write(j);
+                      j := j + 1;
+                    end;
+                    WriteLn;
+                    i := i + 1;
+              end;
 
-      end;
+              // lower half of diamond
+              i := rows - 1;
+              while i >= 1 do
+                begin
+                  j := 1;
+                  while j <= rows - i do
+                    begin
+                      Write(' ');
+                      j := j + 1;
+                    end;
+                    // print numbers
+                    j := 1;
+                    while j <= 2 * i - 1 do
+                      begin
+                        Write(j);
+                        j := j + 1;
+                      end;
+                      WriteLn;
+                      i := i - 1;
+                end;
+          end;
+    end;
     '3': begin
       // Pyramid
-      WriteLn('Enter number of rows: ');
-      ReadLn(rows);
-      j := 1;
-      while j <= rows do
+     Writeln('Choose stars or numbers');
+     WriteLn('1. stars');
+     WriteLn('2. numbers');
+     ReadLn(option2);
+      if option2 = '1' then
         begin
-          i := 1;
-          while i <= rows - j do
-            begin
-              Write(' ');
-              i := i + 1;
-            end;
-          i := 1;
-          while i <= 2 * j - 1 do
-            begin
-              Write('*');
-              i := i + 1;
-            end;
-          WriteLn;
-          j := j + 1;
-        end;
+            WriteLn('Enter the number of rows : '); ReadLn(rows);
+            // create pyramid
+           
+            for i := 1 to rows do 
+              begin
+                for j:= 1 to rows - i do 
+                  Write(' '); 
+
+                  for j := 1 to 2 * i - 1 do
+                    Write('*');
+                    WriteLn;
+              end;
+              ReadLn;
+         end
+         else if option2 = '2' then
+           begin
+             WriteLn('Enter the rows : '); ReadLn(rows);
+              // print spaces
+             for i := 1 to rows do
+               begin
+                 for j := 1 to rows - i do 
+                  Write(' '); 
+                // print number
+                  for j := 1 to 2 * i - 1 do
+                    Write(j);
+                    WriteLn;
+               end;
+               readln;
+           end
+           else 
+           WriteLn('Invalid option');
       end;
     '4': begin
       // Rhombus
